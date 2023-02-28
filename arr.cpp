253d66fd192e27257e649c863f3f42d0e4fda50f
Дмитрий <@ADMIN>
2023-02-28 16:28:12 +0500

    Dz-FinktionNew

--- arr.cpp
+++ arr.cpp
@@ -9,50 +9,63 @@
 double mean(double num1, double num2) {
     return (num1 + num2) / 2;
 }
-
-double mean_arr(double arr[], const int len) {
-    double sum = 0;
-    
-    for (int i = 0; i < len; i++) {
+double mean_arr(int arr[], const int length) {
+    int sum = 0;
+    for (int i = 0; i < length; i++)
         sum += arr[i];
-       
-        return sum/len;
+    return (double)sum / length;
+}
+int  powers(int N, int M) {
+    int num = 0;
+    for (int i = 0; i <= M; i++) {
+        num = pow(N, M);
+        //std::cout << "Power: " << '\n';
+        return num;
     }
 }
 
-
 int main(){
-	setlocale(LC_ALL, "Rus");    
-   
+	setlocale(LC_ALL, "Rus");   
+    int n, m;
+
         /*Task 1
         Create a mean function that returns
          the arithmetic mean of the two numbers
          passed into it.
-        Don't forget to show it works
-        by calling functio,n in the main block.*/
-        /*std::cout<<"Inter the 2 namder -> ";
-        int n, m;
-        std::cin>>n>>m;
-        std::cout <<"The arithmetic mean of the two number "<<mean(n, m);
-        */
+         Don't forget to show it works
+         by calling functio,n in the main block.
+         */
+         /*
+         std::cout<<"Let's derive the arithmetic mean from the sum of the two nambers.\n\n";
+         std::cout<<"Inter the 2 namder -> ";
+         int n, m;
+         std::cin>>n>>m;
+         std::cout <<"The arithmetic mean of the two number "<<mean(n, m);
+         */
         /* Task 2
          Create a mean_arr function.
          It accepts array and its length,
              after which it returns the average
              arithmetic of all its elements.
         */
+        /*
+        std::cout<<"Let's derive the arithmetic mean from the sum of the numbers in the array.\n\n";
         const int size = 4;
         std::cout << "Inter the four namber -> \n";
-        double arr1[size]{};
+        int arr1[size]{};
         std::cin >> arr1[0] >> arr1[1] >> arr1[2] >> arr1[3];
-        std::cout << "The average arithmetic of all its elements -> ";
-       /* for (int i = 0; i < size; i++) {*/
-            //std::cout << arr1[i] << "\t";
-          //  mean_arr(arr1, size);
-            std::cout << mean_arr(arr1, size) << "\t";
-        
-        
-
+        std::cout << "The average arithmetic of all its elements -> "<< mean_arr(arr1, size) << "\t";
+        */
+        /*Task 3
+            Create a function powers(N, M) that
+            takes two numeric values.The function should
+            print to the console all powers of N from power 0 to
+            degree M inclusive.
+         */
+        std::cout << "Task 3 \nRaise H to the power of M and output each value.\n\n";
+        std::cout << "Inter the two numeric values: \n";
+        std::cin >> n >> m;
+        std::cout << powers(n, m);
 
 
 
